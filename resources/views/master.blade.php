82a1bf0f5c2d1cbea390727ff5ae1a2e3a7b05ae
Parth Kakadiya <parthkakadiya320@gmail.com>
2022-12-29 21:53:39 +0100

    Product detail page Done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -35,6 +35,23 @@
     .slider-text {
         background-color: #35443585 !important
     }
+
+    .trending-image {
+        height: 100px;
+    }
+
+    .trending-item {
+        float: left;
+        width: 20%
+    }
+
+    .trending-wrapper {
+        margin: 30px
+    }
+
+    .detail-img {
+        height: 200px;
+    }
 </style>
 
 </html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -13,11 +13,14 @@
             <div class="carousel-inner">
                 @foreach ($products as $item)
                     <div class="item {{ $item['id'] == 1 ? 'active' : '' }}">
-                        <img class="slider-img" src={{ $item['gallery'] }}>
-                        <div class="carousel-caption slider-text">
-                            <h3>{{ $item['name'] }}</h3>
-                            <p>{{ $item['description'] }}</p>
-                        </div>
+                        <a href="detail/{{ $item['id'] }}">
+
+                            <img class="slider-img" src={{ $item['gallery'] }}>
+                            <div class="carousel-caption slider-text">
+                                <h3>{{ $item['name'] }}</h3>
+                                <p>{{ $item['description'] }}</p>
+                            </div>
+                        </a>
                     </div>
                 @endforeach
             </div>
@@ -31,6 +34,19 @@
                 <span class="glyphicon glyphicon-chevron-right"></span>
                 <span class="sr-only">Next</span>
             </a>
+        </div>
+        <div class="trending-wrapper">
+            <h3>Trending Products</h3>
+            @foreach ($products as $item)
+                <a href="detail/{{ $item['id'] }}">
+                    <div class="trending-item">
+                        <img class="trending-image" src={{ $item['gallery'] }}>
+                        <div class="">
+                            <h3>{{ $item['name'] }}</h3>
+                        </div>
+                    </div>
+                </a>
+            @endforeach
         </div>
     </div>
 @endsection
