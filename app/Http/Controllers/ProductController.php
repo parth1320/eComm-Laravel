5ecb5ed0a5c000e7180d7f22888c244615de6ce5
Parth Kakadiya <parthkakadiya320@gmail.com>
2022-12-30 00:47:42 +0100

    Cart Functionalities Done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
+
 
 class ProductController extends Controller
 {
@@ -18,5 +21,23 @@ class ProductController extends Controller
     {
         $data= Product::find($id);
         return view('detail', ['product' => $data]);
+    }
+    function addToCart(Request $req) {
+        if ($req->session()->has('user')) {
+            $cart = new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+
+
+        }
+        else{
+            return redirect('/login');
+        }
+    }
+    function cartItem() {
+        $user_id = Session::get('user')['id'];
+        return Cart::where('user_id', $user_id)->count();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,10 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::get('/login', function () {
-    return view('login');
+return view('login');
 });
 
 Route::post('/login', [UserController::class, 'login']);
-
 Route::get('/', [ProductController:: class, 'index']);
-
 Route::get('detail/{id}', [ProductController::class, 'detail']);
+Route::post('add_to_cart', [ProductController::class, 'addToCart']);
